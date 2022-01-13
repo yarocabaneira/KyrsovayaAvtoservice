ae9ca68b51396a8ca707cabd3dd4c27fe708b7ad
yarocabaneira <isavkin03@mail.ru>
2022-01-13 20:52:03 +0400

    git add

---
+++ KyrsovayaAvtoservise/KyrsovayaAvtoservise/Pages/PageProduct.xaml.cs
@@ -0,0 +1,43 @@
+﻿using KyrsovayaAvtoservise.AppData;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Timers;
+using System.Windows.Threading;
+
+
+namespace KyrsovayaAvtoservise.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для PageProduct.xaml
+    /// </summary>
+    public partial class PageProduct : Page
+    {
+        public PageProduct()
+        {
+            InitializeComponent();
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += UpdateData;
+            timer.Start();
+        }
+
+        public void UpdateData(object sender, object e)
+        {
+            var HistoryProduct = Conect.conObj.Product.ToList();
+            ListProduct.ItemsSource = HistoryProduct;
+            ListProduct.ItemsSource = Conect.conObj.Product.Where(x => x.Title.StartsWith(TxtSearch.Text)|| x.Description.StartsWith(TxtSearch.Text)).ToList();
+        }
+    }
+}
